0f466913200dc8230a08a3f521fd3e019c67e0e9
zeapsu <zeapsu@users.noreply.github.com>
2023-10-30 03:37:25 +0000

    Prettified Code!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
-    <p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
-            </p>
-    <a
-              className="App-link"
+        </p>
+        <a
+          className="App-link"
           href="https://reactjs.org"
-      target="_blank"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-
       </header>
     </div>
   );
