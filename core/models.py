6df6415832d1eb98d4b7f8a476e0911f3d8464f2
OBenjOne <bschoolland@gmail.com>
2024-01-11 00:32:13 -0800

    feat: Add a standard wagtail page to the database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+class StandardPage(Page):
+    subtitle = models.CharField(max_length=250)
+    # next, add the body, a rich text inside of a streamfield
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('body'),
+    ]
