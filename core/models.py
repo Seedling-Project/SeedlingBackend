b76e1ffc74a623f8848b5cd872370a6076c127f5
OBenjOne <bschoolland@gmail.com>
2024-01-11 17:58:20 -0800

    feat: convert body to streamfield

--- core/models.py
+++ core/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
+
+from wagtail.fields import StreamField
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
 
 class StandardPage(Page):
     subtitle = models.CharField(max_length=250)
     # next, add the body, a rich text inside of a streamfield
-    body = RichTextField()
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+    ], use_json_field=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
