2fb53c1661977ce756c97284c17621c195991d73
Kyle Zheng <zhengkyle811@gmail.com>
2024-01-14 15:21:08 -0800

    Research Page Baseline has been created, just need to make sure rendering variable and look is correct.

--- frontend/src/components/Research.tsx
+++ frontend/src/components/Research.tsx
@@ -2,31 +2,49 @@ import React from "react";
 
 const Research: React.FC = () => {
     return (<>
-            <div className="collapse collapse-arrow bg-base-200">
+            <div className="collapse collapse-arrow bg-base-200" id="researchGeneral">
                 <input type="radio" name="my-accordion-2" checked={true}/>
                 <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
+                    General Information
                 </div>
                 <div className="collapse-content">
-                    <p>hello</p>
+                    <p>This is supposed to be from the database</p>
                 </div>
             </div>
-            <div className="collapse collapse-arrow bg-base-200">
+            <div className="collapse collapse-arrow bg-base-200" id="researchLinks">
                 <input type="radio" name="my-accordion-2"/>
                 <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
+                    Links to Opportunities
                 </div>
                 <div className="collapse-content">
-                    <p>hello</p>
+                    <p>This is supposed to be from the database</p>
                 </div>
             </div>
-            <div className="collapse collapse-arrow bg-base-200">
+            <div className="collapse collapse-arrow bg-base-200" id="researchEtap">
                 <input type="radio" name="my-accordion-2"/>
                 <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
+                    Guide to Using NSF Etap Website for REU Applications (etap.nsf.gov)
                 </div>
                 <div className="collapse-content">
-                    <p>hello</p>
+                    <p>This is supposed to be from the database</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200" id="researchSULI">
+                <input type="radio" name="my-accordion-2"/>
+                <div className="collapse-title text-xl font-medium">
+                    SULI Application Guide
+                </div>
+                <div className="collapse-content">
+                    <p>This is supposed to be from the database</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200" id="researchCold">
+                <input type="radio" name="my-accordion-2"/>
+                <div className="collapse-title text-xl font-medium">
+                    Cold Emailing Professors Guide
+                </div>
+                <div className="collapse-content">
+                    <p>This is supposed to be from the database</p>
                 </div>
             </div>
         </>
