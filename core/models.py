949fc2c4967a01557b85e2aca2fc79dd343b38f8
OBenjOne <bschoolland@gmail.com>
2024-01-14 13:38:42 -0800

    add document model

--- core/models.py
+++ core/models.py
@@ -9,7 +9,6 @@ from wagtail.api import APIField
 
 class StandardPage(Page):
     subtitle = models.CharField(max_length=250)
-    # next, add the body, a rich text inside of a streamfield
     body = StreamField([
         ('heading', blocks.CharBlock(form_classname="title")),
         ('paragraph', blocks.RichTextBlock()),
@@ -24,3 +23,27 @@ class StandardPage(Page):
         'subtitle',
         'body',
     ]
+
+class documentPage(Page):
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    date = models.DateField("Post date")
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('author'),
+        FieldPanel('date'),
+        FieldPanel('body'),
+    ]
+
+    api_fields = [
+        'subtitle',
+        'author',
+        'date',
+        'body',
+    ]
+    
