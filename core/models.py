800ef976fe36d4e12c7c8bd27db7b6f93062c41e
Kyle Zheng <zhengkyle811@gmail.com>
2024-01-13 23:37:23 -0800

    New Blog model

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from wagtail.fields import StreamField
 from wagtail.models import Page
 from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
 class StandardPage(Page):
     subtitle = models.CharField(max_length=250)
@@ -16,5 +17,19 @@ class StandardPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
+        FieldPanel('body'),
+    ]
+class BlogPage(Page):
+    author = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('author'),
+        FieldPanel('date'),
         FieldPanel('body'),
     ]
